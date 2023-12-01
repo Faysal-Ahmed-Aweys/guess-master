2526c61ebbf70880d53b7a9c03d982f4a8421894
Faysal Aweys <Faysalgeesle@hotmail.com>
2023-12-01 14:05:32 +0000

    Import random and test

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+import random
+
+randNumber = print(random.randint(0,10))
+
 def ask_for_a_number():
     while True:
         try:
@@ -9,19 +13,20 @@ def ask_for_a_number():
             print("You should input a number")
     return number
 
+
 integer = ask_for_a_number()
 print(f"Your number is {integer}")
 
 def try_again_():
     while True:
-            try_again = input("Would you like to try again? y/n ")
-            if try_again.lower() == "y":
-                integer = ask_for_a_number()
-                print(f"Your number is {integer}")
-            elif try_again.lower() == "n":
-                print("Game over")
-                break
-            else:
-                print("You should input either y/n")
+        try_again = input("Would you like to try again? y/n ")
+        if try_again.lower() == "y":
+            integer = ask_for_a_number()
+            print(f"Your number is {integer}")
+        elif try_again.lower() == "n":
+            print("Game over")
+            break
+        else:
+            print("You should input either y/n")
 
 try_again_()
