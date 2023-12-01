9c7468456d0d9c3d455914e1b873c02cf6b370d9
Faysal Aweys <Faysalgeesle@hotmail.com>
2023-12-01 17:26:36 +0000

    Improve game style and tidy up the code

--- run.py
+++ run.py
@@ -29,10 +29,9 @@ def ask_for_a_number(rand_number):
         try:
             number = int(input("Guess a number (between 0 and 10):\n"))
             if type(number) == int and number == rand_number:
-                print("Okay, You're right. Well done!!")
+                print("You got it, Well done!!")
                 print(f"The number is {rand_number}")
                 try_again_()
-                break
             elif type(number) == int and number < rand_number and attempts != 1:
                 attempts -= 1
                 print("Higher")
@@ -44,15 +43,11 @@ def ask_for_a_number(rand_number):
             elif attempts <= 1:
                 attempts -= 1
                 print(f"{attempts} chance(s) left")
-                print("game over")
+                print("Game over")
                 print(f"The number is {rand_number}")
                 try_again_()
-                break
         except ValueError:
             print("You should input a number")
-        if attempts <= 0:
-            print("game over")
-            break
 
 
 def generate_random_number():
@@ -69,11 +64,11 @@ def game_rules():
 - The computer is thinking of a number and it will tell you the range.
 - Your job is to guess what the number is.
 - If you guess a lower number, the computer will tell you higher which means
-you need to guess a number higher than the number you guessed.
+  you need to guess a number higher than the number you guessed.
 - If you guess a higher number, the computer will tell you lower which means
-you need to guess a number lower than the number you guessed.
+  you need to guess a number lower than the number you guessed.
 - If you guess the right number you win!
-- But, you have only three chances to guess what the number is!
+- You have only three chances to guess what the number is!
 
                     HAVE FUN!!!
     """)
@@ -94,11 +89,14 @@ you need to guess a number lower than the number you guessed.
 
 
 def welcome():
-    print("""
-╔═╗┬ ┬┌─┐┌─┐┌─┐  ┌┬┐┬ ┬┌─┐  ┌┐┌┬ ┬┌┬┐┌┐ ┌─┐┬─┐  
-║ ╦│ │├┤ └─┐└─┐   │ ├─┤├┤   ││││ ││││├┴┐├┤ ├┬┘  
-╚═╝└─┘└─┘└─┘└─┘   ┴ ┴ ┴└─┘  ┘└┘└─┘┴ ┴└─┘└─┘┴└─                                                                                            
-    """)
+
+    row1 = "╔═╗┬ ┬┌─┐┌─┐┌─┐  ┌┬┐┬ ┬┌─┐  ┌┐┌┬ ┬┌┬┐┌┐ ┌─┐┬─┐"
+    row2 = "║ ╦│ │├┤ └─┐└─┐   │ ├─┤├┤   ││││ ││││├┴┐├┤ ├┬┘"
+    row3 = "╚═╝└─┘└─┘└─┘└─┘   ┴ ┴ ┴└─┘  ┘└┘└─┘┴ ┴└─┘└─┘┴└─"
+
+    print(row1.center(100))
+    print(row2.center(100))
+    print(row3.center(100))
 
     print("""
 SELECT AN OPTION:
@@ -108,7 +106,7 @@ SELECT AN OPTION:
 
     while True:
         try:
-            chosen_option = int(input("type 1 or 2:\n"))
+            chosen_option = int(input("Type 1 or 2:\n"))
             if type(chosen_option) == int and chosen_option == 1:
                 random_number = generate_random_number()
                 ask_for_a_number(random_number)
@@ -117,7 +115,7 @@ SELECT AN OPTION:
                 game_rules()
                 break
         except ValueError:
-            print("please type 1 or 2")
+            print("Please type 1 or 2")
 
 
 welcome()
