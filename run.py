eaef3c77ee2fae4dd06e63abfc1e92745f01ae01
Faysal Aweys <Faysalgeesle@hotmail.com>
2023-12-01 14:54:13 +0000

    Rearrange functions and add attempts to the game

--- run.py
+++ run.py
@@ -1,39 +1,72 @@
 import random
 
-def generate_random_number():
-    randNumber = int(random.randint(0, 10))
-    return randNumber
 
-def ask_for_a_number(randNumber):
+def try_again_():
+    while True:
+        try_again = input("Would you like to try again? y/n ")
+        if try_again.lower() == "y":
+            random_number = generate_random_number()
+            print(random_number)
+            ask_for_a_number(random_number)
+        elif try_again.lower() == "n":
+            welcome()
+            break
+        else:
+            print("You should input either y/n")
+
+
+def ask_for_a_number(rand_number):
+    attempts = 3
+    print(f"{attempts} chance(s) left")
     while True:
         try:
-            number = int(input("Guess a number (it is between 0 and 10): "))
-            if type(number) == int and number == randNumber:
+            number = int(input("Guess a number (between 0 and 10): "))
+            if type(number) == int and number == rand_number:
                 print("Okay, You're right. Well done!!")
-                print(f"The number is {randNumber}")
+                print(f"The number is {rand_number}")
+                try_again_()
                 break
-            elif type(number) == int and number < randNumber:
+            elif type(number) == int and number < rand_number and attempts != 1:
+                attempts -= 1
                 print("Higher")
-            elif type(number) == int and number > randNumber:
+                print(f"{attempts} chance(s) left")
+            elif type(number) == int and number > rand_number and attempts != 1:
+                attempts -= 1
                 print("lower")
+                print(f"{attempts} chance(s) left")
+            elif attempts <= 1:
+                attempts -= 1
+                print(f"{attempts} chance(s) left")
+                print("game over")
+                print(f"The number is {rand_number}")
+                try_again_()
+                break
         except ValueError:
             print("You should input a number")
-    return number
+        if attempts <= 0:
+            print("game over")
+            break
 
 
-def gameRules():
+def generate_random_number():
+    rand_number = int(random.randint(0, 10))
+    return rand_number
+
+
+def game_rules():
     print("""
                         Game Rules
                         ----------
 
-    - The game rules are simple and straightforward.
-    - The computer is thinking of a number, it will tell you the range.
-    - Your job is to guess what the number is. 
-    - If you guess a lower number, the computer will tell you higher which means 
-      you need to guess a number higher than the number you guessed. 
-    - If you guess a higher number, the computer will tell you lower which means 
-      you need to guess a number lower than the number you guessed.
-    - here is the catch though, you have only three chances to guess what the number is!
+- The game rules are simple and straightforward.
+- The computer is thinking of a number and it will tell you the range.
+- Your job is to guess what the number is.
+- If you guess a lower number, the computer will tell you higher which means
+you need to guess a number higher than the number you guessed.
+- If you guess a higher number, the computer will tell you lower which means
+you need to guess a number lower than the number you guessed.
+- If you guess the right number you win!
+- But, you have only three chances to guess what the number is!
 
                     HAVE FUN!!!
     """)
@@ -41,7 +74,9 @@ def gameRules():
     while True:
         start_game = input("Would you like to start the game? y/n ")
         if start_game.lower() == "y":
-            generate_random_number()
+            random_number = generate_random_number()
+            print(random_number)
+            ask_for_a_number(random_number)
             break
         elif start_game.lower() == "n":
             welcome()
@@ -52,43 +87,29 @@ def gameRules():
 
 def welcome():
     print("""
-    ~~~ Hello and welcome ~~~
+    ~~~ Hello and welcome to guess the number game ~~~
     """)
 
     print("""
 select an option:
-1. Start game 
+1. Start game
 2. Game rules
     """)
+
     while True:
         try:
             chosen_option = int(input("type 1 or 2: "))
-            type(chosen_option) == int
             if type(chosen_option) == int and chosen_option == 1:
-                generate_random_number()
+                random_number = generate_random_number()
+                print(random_number)
+                ask_for_a_number(random_number)
                 break
             elif type(chosen_option) == int and chosen_option == 2:
-                gameRules()
+                game_rules()
                 break
         except ValueError:
-            print("please enter a number")
+            print("please type 1 or 2")
+
 
 welcome()
-random_number = generate_random_number()
-print(random_number)
-integer = ask_for_a_number(random_number)
 
-def try_again_():
-    while True:
-        try_again = input("Would you like to try again? y/n ")
-        if try_again.lower() == "y":
-            random_number = generate_random_number()
-            print(random_number)
-            ask_for_a_number(random_number)
-        elif try_again.lower() == "n":
-            print("game over")
-            break
-        else:
-            print("You should input either y/n")
-
-try_again_()
