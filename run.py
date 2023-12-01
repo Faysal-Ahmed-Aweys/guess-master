9f490714bc29bf21a37154c223fa52db69d267b5
Faysal Aweys <Faysalgeesle@hotmail.com>
2023-12-01 14:15:12 +0000

    Add generate_random_number function and update ask_for_a_number function

--- run.py
+++ run.py
@@ -1,30 +1,38 @@
 import random
 
-randNumber = print(random.randint(0,10))
+def generate_random_number():
+    randNumber = int(random.randint(0, 10))
+    return randNumber
 
-def ask_for_a_number():
+def ask_for_a_number(randNumber):
     while True:
         try:
-            number = int(input("What is your number?: "))
-            if type(number) == int:
-                print("Okay")
+            number = int(input("Guess a number (it is between 0 and 10): "))
+            if type(number) == int and number == randNumber:
+                print("Okay, You're right. Well done!!")
+                print(f"The number is {randNumber}")
                 break
+            elif type(number) == int and number < randNumber:
+                print("Higher")
+            elif type(number) == int and number > randNumber:
+                print("lower")
         except ValueError:
             print("You should input a number")
     return number
 
-
-integer = ask_for_a_number()
-print(f"Your number is {integer}")
+random_number = generate_random_number()
+print(random_number)
+integer = ask_for_a_number(random_number)
 
 def try_again_():
     while True:
         try_again = input("Would you like to try again? y/n ")
         if try_again.lower() == "y":
-            integer = ask_for_a_number()
-            print(f"Your number is {integer}")
+            random_number = generate_random_number()
+            print(random_number)
+            ask_for_a_number(random_number)
         elif try_again.lower() == "n":
-            print("Game over")
+            print("You're done")
             break
         else:
             print("You should input either y/n")
